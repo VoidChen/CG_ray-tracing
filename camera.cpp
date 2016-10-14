6b2db73eccbca2f2daf2ffa88e4cd6921406ff18
VoidChen <void.tw@gmail.com>
2016-10-14 20:05:23 +0800

    Simple camera with ppm output

--- camera.cpp
+++ camera.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include"obj.h"
+#include"ppm.h"
 using namespace std;
 
 class camera{
@@ -11,6 +12,7 @@ class camera{
         vec3 horizontal;
         vec3 vertical;
         ray** primary;
+        vec3** color;
 
         camera(){
         }
@@ -29,10 +31,14 @@ class camera{
             vec3 unit_v = vertical / height;
 
             primary = new ray*[height];
+            color = new vec3*[height];
             for(int i = 0; i < height; ++i){
                 primary[i] = new ray[width];
-                for(int j = 0; j < width; ++j)
+                color[i] = new vec3[width];
+                for(int j = 0; j < width; ++j){
                     primary[i][j] = ray(origin, (lowerleft + unit_h * (j+0.5) + unit_v * (i+0.5) - origin));
+                    color[i][j] = vec3(255, 255, 255);
+                }
             }
         }
 };
@@ -43,18 +49,24 @@ void print_vec3(vec3 v){
 
 int main(){
     camera C;
+    sphere S;
+    double t;
 
-    C = camera(vec3(0, 0, 0), 200, 100, vec3(-2, -1, -1), vec3(4, 0, 0), vec3(0, 2, 0));
+    C = camera(vec3(0, 0, 0), 1200, 600, vec3(-2, -1, -1), vec3(4, 0, 0), vec3(0, 2, 0));
+    S = sphere(vec3(1, 2, -10), 3);
     for(int i = 0; i < C.height; ++i){
-        for(int j = 0; j < C.width; ++j)
-            print_vec3(C.primary[i][j].direction);
+        for(int j = 0; j < C.width; ++j){
+            t = S.hit(C.primary[i][j]);
+            if(t != -1)
+                C.color[i][j] = vec3(255, 255, 0);
+        }
     }
 
-    C = camera(vec3(0, 0, 0), 5, 3, vec3(-2, -1, -1), vec3(4, 0, 0), vec3(0, 2, 0));
-    for(int i = 0; i < C.height; ++i){
-        for(int j = 0; j < C.width; ++j)
-            print_vec3(C.primary[i][j].direction);
-    }
+    ppm image;
+
+    image = ppm(C.width, C.height);
+    image.set_color(C.color);
+    image.output("test.ppm");
 
     return 0;
 }
