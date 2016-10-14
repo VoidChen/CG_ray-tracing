7c0a94ee548f05f169394fab52ec35da738dfa63
VoidChen <void.tw@gmail.com>
2016-10-14 20:58:19 +0800

    Separate camera.cpp

--- camera.h
+++ camera.h
@@ -1,7 +1,5 @@
-#include<iostream>
 #include"obj.h"
 #include"ppm.h"
-using namespace std;
 
 class camera{
     public:
@@ -42,36 +40,3 @@ class camera{
             }
         }
 };
-
-void print_vec3(vec3 v){
-    cout << "(" << v.x << ", " << v.y << ", " << v.z << ")"<<endl;
-}
-
-int main(){
-    camera C;
-    sphere S;
-    vec3 light;
-    double t, diffuse;
-
-    C = camera(vec3(0, 0, 0), 1200, 600, vec3(-2, -1, -1), vec3(4, 0, 0), vec3(0, 2, 0));
-    S = sphere(vec3(1, 2, -15), 5);
-    light = vec3(-10, 15, -2);
-    for(int i = 0; i < C.height; ++i){
-        for(int j = 0; j < C.width; ++j){
-            t = S.hit(C.primary[i][j]);
-            if(t != -1){
-                vec3 hit_point = C.primary[i][j].point(t);
-                diffuse = vec3::dot((hit_point - S.center).unit(), (light - hit_point).unit());
-                C.color[i][j] = vec3(255, 255, 255) * diffuse;
-            }
-        }
-    }
-
-    ppm image;
-
-    image = ppm(C.width, C.height);
-    image.set_color(C.color);
-    image.output("output.ppm");
-
-    return 0;
-}
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include"camera.h"
+using namespace std;
+
+int main(){
+    //init scene
+    camera C;
+    sphere S;
+    vec3 light;
+    double t, diffuse;
+
+    C = camera(vec3(0, 0, 0), 1200, 600, vec3(-2, -1, -1), vec3(4, 0, 0), vec3(0, 2, 0));
+    S = sphere(vec3(1, 2, -15), 5);
+    light = vec3(-10, 15, -2);
+
+    //render
+    for(int i = 0; i < C.height; ++i){
+        for(int j = 0; j < C.width; ++j){
+            t = S.hit(C.primary[i][j]);
+            if(t != -1){
+                vec3 hit_point = C.primary[i][j].point(t);
+                diffuse = vec3::dot((hit_point - S.center).unit(), (light - hit_point).unit());
+                C.color[i][j] = vec3(255, 255, 255) * diffuse;
+            }
+        }
+    }
+
+    //genarate ppm
+    ppm image;
+    image = ppm(C.width, C.height);
+    image.set_color(C.color);
+    image.output("output.ppm");
+
+    return 0;
+}
